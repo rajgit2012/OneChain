085ad1754e8736501c57088b84577355d0a5f5d5
rajgit2012 <rajnsu08@gmail.com>
2016-10-22 16:07:05 +0100

    inject  modules

---
+++ app/src/main/java/com/mangolab/onechain/dagger/components/modules/DaggerImplementor.java
@@ -0,0 +1,19 @@
+package com.mangolab.onechain.dagger.components.modules;
+
+import java.util.List;
+
+/**
+ * Created by rajforhad on 22/10/2016.
+ */
+
+public class DaggerImplementor implements IDagger {
+    @Override
+    public void injectNewItem() {
+
+    }
+
+    @Override
+    public List<String> allInjectedItems() {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/mangolab/onechain/dagger/components/modules/IDagger.java
@@ -0,0 +1,12 @@
+package com.mangolab.onechain.dagger.components.modules;
+
+import java.util.List;
+
+/**
+ * Created by rajforhad on 22/10/2016.
+ */
+
+public interface IDagger {
+    void injectNewItem();
+    List<String> allInjectedItems();
+}
