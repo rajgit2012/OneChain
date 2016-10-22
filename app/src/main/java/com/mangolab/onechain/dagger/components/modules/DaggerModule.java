b823a6ab3c41662151cdd1cc2d1805b7a2a73543
rajgit2012 <rajnsu08@gmail.com>
2016-10-22 02:06:28 +0100

    added modules

---
+++ app/src/main/java/com/mangolab/onechain/dagger/components/modules/DaggerModule.java
@@ -0,0 +1,12 @@
+package com.mangolab.onechain.dagger.components.modules;
+
+/**
+ * Created by rajforhad on 22/10/2016.
+ */
+
+public class DaggerModule {
+
+    public void provideApi(){
+
+    }
+}
